9a998850ecfc10a3648f231c55137d71f2332f16
Cem <cemyilmaz185@gmail.com>
2025-04-27 09:02:50 +0100

    updating cors policy on rails

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'localhost:3001', 'localhost:3002', '127.0.0.1:3000', '127.0.0.1:3001', '127.0.0.1:3002'
+    origins 'localhost:3000', 'localhost:3001', 'localhost:3002', '127.0.0.1:3000', '127.0.0.1:3001', '127.0.0.1:3002', 
+           'http://54.84.82.77:3002', '54.84.82.77:3002', 'http://54.84.82.77:3001', '54.84.82.77:3001'
 
     resource '*',
       headers: :any,
